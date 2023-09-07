88a72017083cd94feac55f820bebe5f5ee72d515
Afolabi Esther <starafolab@gmail.com>
2023-09-07 04:34:39 +0100

    Add time & day

---
+++ src/main.js
@@ -0,0 +1,38 @@
+const timeEl = document.querySelector(".time");
+const dayEl = document.querySelector(".day");
+
+let date = new Date();
+
+timeEl.textContent = date.toLocaleTimeString("en-US");
+
+// DAY NAME
+let dayName = date.getDay();
+//   dayNum = today.getDate(),
+ let  month = date.getMonth();
+ let year = date.getFullYear();
+
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+const week = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+dayEl.textContent = `${week[dayName]}, ${months[month]} ${year}`;
